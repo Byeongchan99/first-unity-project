49c63f060969aa3f9a406977fe5cec5b3731e114
Byeongchan99 <harrrypoter@naver.com>
2023-08-30 08:29:07 +0900

    근접 공격 이펙트

--- Assets/Scripts/PlayerAttackArea.cs
+++ Assets/Scripts/PlayerAttackArea.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class PlayerAttackArea : BaseAttackArea
 {
+    public GameObject trailParentObject; // 새로운 부모 오브젝트 참조
+    private TrailRenderer trailRenderer;
+    private List<Vector2> points = new List<Vector2>();
+
+    void Awake()
+    {
+        if (trailParentObject)
+            trailRenderer = trailParentObject.GetComponentInChildren<TrailRenderer>(); // 자식 오브젝트에서 트레일 렌더러를 찾습니다.
+
+        if (trailRenderer)
+            trailRenderer.enabled = false;
+    }
+
+    public override void ActivateAttackRange(Vector2 attackDirection, float weaponRange)
+    {
+        base.ActivateAttackRange(attackDirection, weaponRange);
+
+        if (trailRenderer)
+            trailRenderer.enabled = true;
+
+        // 트레일 부모 오브젝트 회전
+        if (trailParentObject != null)
+        {
+            float angle = Mathf.Atan2(attackDirection.y, attackDirection.x) * Mathf.Rad2Deg;
+            trailParentObject.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+        }
+    }
+
     public override void CalculateColiderPoints(float radius)
     {
-        List<Vector2> points = new List<Vector2>();
+        points.Clear();
         int resolution = 30;
 
-        // 원의 중심에 대한 점 추가
         points.Add(Vector2.zero);
 
-        // 시작 각도와 끝 각도를 라디안으로 설정
-        float startAngle = Mathf.PI / 18;    // 10 degrees in radians
-        float endAngle = 17 * Mathf.PI / 18; // 170 degrees in radians
-
+        float startAngle = Mathf.PI / 18;
+        float endAngle = 17 * Mathf.PI / 18;
         float deltaAngle = (endAngle - startAngle) / resolution;
 
         for (int i = 0; i <= resolution; i++)
@@ -25,5 +50,19 @@ public class PlayerAttackArea : BaseAttackArea
         }
 
         attackRangeCollider.SetPath(0, points);
+        StartCoroutine(MoveTrailObject());
+    }
+
+    private IEnumerator MoveTrailObject()
+    {
+        foreach (Vector2 point in points)
+        {
+            if (trailRenderer)
+                trailRenderer.transform.localPosition = point;
+            yield return null;
+        }
+
+        if (trailRenderer)
+            trailRenderer.enabled = false;
     }
 }
