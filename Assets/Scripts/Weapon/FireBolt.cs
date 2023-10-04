55ae616d0e05be9a155b08c70536945da3b8b0b3
Byeongchan99 <harrrypoter@naver.com>
2023-10-05 04:03:58 +0900

    파이어볼트 정상 작동

--- Assets/Scripts/Weapon/FireBolt.cs
+++ Assets/Scripts/Weapon/FireBolt.cs
@@ -17,7 +17,6 @@ public class FireBolt : Bullet
 
     [Header("Explosion Animation")]
     private Animator explosionAnimator;  // 파이어볼트의 애니메이터 참조
-    private SpriteRenderer fireBoltSpriteRenderer; // 파이어볼트 스프라이트 렌더러 참조
 
     private new void Awake()
     {
@@ -25,10 +24,6 @@ public class FireBolt : Bullet
         GameObject explosionArea = transform.Find("ExplosionArea").gameObject;
         explosionCollider = explosionArea.GetComponent<CircleCollider2D>();
         explosionAnimator = explosionArea.GetComponent<Animator>(); // 폭발 애니메이터 초기화
-
-        GameObject fireBoltSprite = transform.Find("FireBolt Sprite").gameObject;
-        fireBoltSpriteRenderer = fireBoltSprite.GetComponent<SpriteRenderer>();
-
         explosionCollider.enabled = false;   // 처음에는 비활성화 상태
     }
 
@@ -43,7 +38,6 @@ public class FireBolt : Bullet
         if (explosionAnimator)
         {
             explosionAnimator.ResetTrigger("Hit");
-            explosionAnimator.Play("Empty", 0, 0f);
         }
 
         if (explosionCollider)
@@ -54,11 +48,6 @@ public class FireBolt : Bullet
         if (explosionSpriteRenderer)
         {
             explosionSpriteRenderer.sprite = null;  // 폭발 스프라이트 초기화
-        }
-
-        if (fireBoltSpriteRenderer)
-        {
-            fireBoltSpriteRenderer.enabled = true;
         }
     }
 
@@ -83,7 +72,6 @@ public class FireBolt : Bullet
     {
         // 폭발 시 원 콜라이더 활성화
         explosionCollider.enabled = true;
-        fireBoltSpriteRenderer.enabled = false; // 파이어볼트 스프라이트 비활성화
         // 폭발 애니메이션 실행      
         explosionAnimator.SetTrigger("Hit");
 
