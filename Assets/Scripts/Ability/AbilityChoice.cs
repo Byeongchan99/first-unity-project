c5c787dd949b16a5a480e6e8cebeac9fc5f60286
Byeongchan99 <harrrypoter@naver.com>
2023-10-10 05:09:42 +0900

    어빌리티 마무리

--- Assets/Scripts/Ability/AbilityChoice.cs
+++ Assets/Scripts/Ability/AbilityChoice.cs
@@ -5,7 +5,29 @@ using UnityEngine.UI;
 
 public class AbilityChoice : MonoBehaviour
 {
+    RectTransform rect;
     public AbilityData[] allAbilities; // Unity 에디터에서 직접 참조할 AbilityData 배열
+
+    void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    public void Show()
+    {
+        rect.localScale = Vector3.one;
+        GameManager.instance.Stop();
+        // AudioManager.instance.PlaySfx(AudioManager.Sfx.LevelUp);
+        // AudioManager.instance.EffectBgm(true);
+    }
+
+    public void Hide()
+    {
+        rect.localScale = Vector3.zero;
+        GameManager.instance.Resume();
+        // AudioManager.instance.PlaySfx(AudioManager.Sfx.Select);
+        // AudioManager.instance.EffectBgm(false);
+    }
 
     public void DisplayRandomAbilities()
     {
