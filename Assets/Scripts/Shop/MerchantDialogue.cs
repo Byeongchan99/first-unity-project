980e0560f06dbfb55bb7eb7de98199ab1e3fe9ea
Bychan <harrrypoter@naver.com>
2023-10-25 22:36:25 +0900

    상점 시스템 다듬기

--- Assets/Scripts/Shop/MerchantDialogue.cs
+++ Assets/Scripts/Shop/MerchantDialogue.cs
@@ -13,8 +13,41 @@ public class MerchantDialogue : MonoBehaviour
         public List<string> dialogues;
     }
 
-    // 현재 이 스크립트가 장착된 컴포넌트의 인스펙터창에서 대사 편집 가능 -> 나중에 스크립트로 관리하도록 바꿔도 될 듯
-    public List<Dialogue> allDialogues;
+    public List<Dialogue> allDialogues = new List<Dialogue>();
+
+    private void Awake()
+    {
+        // 예시 대사 추가
+        allDialogues.Add(new Dialogue
+        {
+            situation = "입장",
+            dialogues = new List<string>
+            {
+                "뭐 필요한 거라도 있어?",
+                "포션 한 잔 어때?",
+                "사고 싶으면 \n그냥 사",
+                "마음에 드는 걸 \n찾아봐",
+                "천천히 둘러봐",
+                "뭔가 필요한거야?",
+            }
+        });
+
+        allDialogues.Add(new Dialogue
+        {
+            situation = "거래",
+            dialogues = new List<string>
+            {
+                "좋은 거래였어",
+                "뭔가 더 사갈래?",
+                "구매해줘서 고마워",
+                "잘 선택했어",
+                "환불은 곤란해",
+                "다음에 또 와"
+            }
+        });
+
+        // 다른 상황의 대사도 위와 같은 방식으로 추가 가능
+    }
 
     // 상황에 따라 대사 반환
     public List<string> GetDialoguesForSituation(string currentSituation)
