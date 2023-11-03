28674d8f6126959d0538a728100dd123715d46f8
Byeongchan99 <harrrypoter@naver.com>
2023-11-04 05:43:59 +0900

    보스 몬스터 패턴 1 구현

--- Assets/Scripts/Monster/BossMonster.cs
+++ Assets/Scripts/Monster/BossMonster.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 
 public class BossMonster : MonoBehaviour
 {
+    protected Transform target;   // 플레이어 위치
     private Animator animator; // 애니메이터 컴포넌트
+    public GameObject leftHand; // 왼쪽 팔 오브젝트
+    public GameObject rightHand; // 오른쪽 팔 오브젝트
+
+    public Vector2 originalPositionLeft, originalPositionRight;   // 기존 손의 위치
     private float attackCooldown; // 다음 공격까지의 시간
     private bool isPatternActive = false; // 현재 공격 패턴이 실행 중인지 추적하는 변수
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        target = PlayerStat.Instance.transform;
+
+        // 보스 몬스터의 위치에서 (-2, -2), (2, -2) 이동한 위치를 기존 손의 위치로 설정
+        originalPositionLeft = (Vector2)transform.position + new Vector2(2, -2);
+        originalPositionRight = (Vector2)transform.position + new Vector2(-2, -2);
     }
+
 
     void Update()
     {
@@ -27,42 +38,105 @@ public class BossMonster : MonoBehaviour
         }
     }
 
-
     // 공격 사이의 랜덤 쿨타임
     float GetRandomCooldown()
     {
         return Random.Range(2f, 4f); // 2초에서 5초 사이의 무작위 시간
     }
 
+    // 손을 움직이는 메서드
+    public void MoveHand(GameObject selectedHand, Vector3 startPosition, Vector3 endPosition, float duration)
+    {
+        // 손 이동 코루틴 시작
+        StartCoroutine(MoveArmRoutine(selectedHand, startPosition, endPosition, duration));
+    }
+
+    // 손 이동 코루틴
+    IEnumerator MoveArmRoutine(GameObject hand, Vector3 start, Vector3 end, float time)
+    {
+        float elapsedTime = 0;   // 경과 시간
+
+        while (elapsedTime < time)
+        {
+            // 손의 위치를 시작 위치에서 끝 위치로 선형 보간
+            hand.transform.position = Vector3.Lerp(start, end, (elapsedTime / time));
+
+            // 경과 시간 증가
+            elapsedTime += Time.deltaTime;
+
+            // 다음 프레임까지 대기
+            yield return null;
+        }
+
+        // 최종 위치 설정
+        hand.transform.position = end;
+    }
+
     // 공격 패턴 코루틴
 
-    // 패턴 1: 한 손으로 내리치기, 다른 손으로 방어
+    // 패턴 1: 한 손씩 내려찍기
+    // 한 손으로는 플레이어의 위치를 추적해 내려찍기, 다른 손으로는 시간차를 두고 내려찍기
     IEnumerator Pattern1()
     {
         isPatternActive = true; // 패턴 시작
 
-        // 내리치기 동작
-        // ...
-        Debug.Log("패턴 1");
+        // 플레이어의 위치를 기준으로 손 선택 - 맵의 왼쪽에 있으면 왼손, 오른쪽에 있으면 오른손
+        GameObject firstHand = selectHand();
+        GameObject secondHand = (firstHand == leftHand) ? rightHand : leftHand;
+
+        // 플레이어의 위치로 선택한 손 이동
+        Debug.Log("첫번째 손 이동");
+        MoveHand(firstHand, firstHand.transform.position, target.position, 1f);
+        yield return new WaitForSeconds(1.0f);
+
+        // 내리찍기 동작
+        Debug.Log("첫번째 손 내려찍기");
+        yield return new WaitForSeconds(0.5f);
+
+        // 시간차를 두고 반대손 이동
+        yield return new WaitForSeconds(1.0f);
+        Debug.Log("두번째 손 이동");
+        MoveHand(secondHand, secondHand.transform.position, target.position, 1f);
+        yield return new WaitForSeconds(1.0f);
+
+        // 내려찍기 동작
+        Debug.Log("두번째 손 내려찍기");
+        yield return new WaitForSeconds(0.5f);
+
+        // 손 원래 위치로 복귀
+        Debug.Log("손 복귀");
+        MoveHand(leftHand, leftHand.transform.position, originalPositionLeft, 1f);
+        MoveHand(rightHand, rightHand.transform.position, originalPositionRight, 1f);
+
+        Debug.Log("패턴 1 완료");
         yield return new WaitForSeconds(2.0f); // 2초간 대기
-
-        // 방어 상태 해제
-        // ...
-
         isPatternActive = false; // 패턴 종료
     }
 
-    // 패턴 2: 힘을 모아 전방 휩쓸기 레이저
+    // 손 선택 메서드
+    public GameObject selectHand()
+    {
+        Vector2 Direction = (target.position - transform.position).normalized;
+        if (Direction.x > 0) 
+        {
+            return leftHand;
+        }
+        else
+        {
+            return rightHand;
+        }
+    }
+
+    // 패턴 2: 힘을 모아 전방 휩쓸기 레이저 발사
     IEnumerator Pattern2()
     {
-        // 힘 모으는 동작
-        // ...
+        // 양 손 모두 본체 근처로 이동
+        // 힘 모으는 동작 / 보자기
+
+        // 힘 모으는 동작 후 얼굴을 같이 회전시키며 레이저 발사
 
         Debug.Log("패턴 2");
         yield return new WaitForSeconds(1.5f); // 1.5초간 대기
-
-        // 레이저 발사
-        // ...
         isPatternActive = false; // 패턴 종료
     }
 
@@ -70,28 +144,31 @@ public class BossMonster : MonoBehaviour
     IEnumerator Pattern3()
     {
         int hits = 3; // 충격파를 생성할 횟수
+
         for (int i = 0; i < hits; i++)
         {
-            // 내리치기 동작
-            // ...
+            // 양 손 모두 본체 근처로 이동
 
-            Debug.Log("패턴 3");
-            yield return new WaitForSeconds(0.5f); // 0.5초간 대기
+            // 양 손을 올리고 내려치는 동작
+
+            // 맵을 퍼쳐나가는 충격파 발생
+            
         }
+        Debug.Log("패턴 3");
+        yield return new WaitForSeconds(0.5f); // 0.5초간 대기
         isPatternActive = false; // 패턴 종료
     }
 
     // 패턴 4: 강한 레이저 발사
     IEnumerator Pattern4()
     {
-        // 힘 모으는 동작
-        // ...
+        // 양 손 모두 본체 근처로 이동
+        // 힘 모으는 동작 / 주먹
+
+        // 힘 모으는 동작 후 맵 중앙을 향해 강한 레이저 발사
 
         Debug.Log("패턴 4");
         yield return new WaitForSeconds(2.0f); // 2초간 대기
-
-        // 레이저 발사
-        // ...
         isPatternActive = false; // 패턴 종료
     }
 
@@ -103,23 +180,25 @@ public class BossMonster : MonoBehaviour
 
         while (duration > 0)
         {
-            // 낙석 생성 위치 결정
-            // ...
+            // 힘을 모으는 동작
+
+            // 낙석 생성 위치 랜덤으로 결정
 
             // 낙석 생성
-            // ...
-
-            Debug.Log("패턴 5");
+            
+            // 생성된 낙석은 일정 시간동안 남아있으며 플레이어의 동선을 방해 -> 일정 시간 후 파괴
+         
             yield return new WaitForSeconds(interval);
             duration -= interval;
         }
+        Debug.Log("패턴 5");
         isPatternActive = false; // 패턴 종료
     }
 
     // 공격 패턴을 랜덤으로 선택하여 실행
     public void ExecuteRandomPattern()
     {
-        int pattern = Random.Range(1, 6); // 1부터 5 사이의 랜덤한 숫자
+        int pattern = Random.Range(1, 1); // 1부터 5 사이의 랜덤한 숫자
 
         switch (pattern)
         {
